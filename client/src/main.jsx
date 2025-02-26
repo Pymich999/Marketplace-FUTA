a383510f3c01bcded658ba0695625dcb0401cf8c
pymich999 <bellorejoice839@gmail.com>
2025-02-26 10:30:10 +0100

    added redux state for auth logic, created register page component and fixed major bugs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Provider} from 'react-redux'
+import ReactDom from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import './index.css'
+import store from './app/store.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+<Provider store={store}>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+</Provider>
 )
